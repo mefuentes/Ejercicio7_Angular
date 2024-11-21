d6309eac5f97b7d2607acb43f4f2ec36abefce5b
Mariano Fuentes <marianofu10@gmail.com>
2024-11-20 23:31:29 -0300

    Ejerc7_Angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,29 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { CommonModule } from '@angular/common'; // Para directivas como *ngIf y *ngFor
+import { HttpClientModule } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet],
+  standalone: true, // Indica que es un componente independiente
+  imports: [CommonModule, HttpClientModule], // Aquí se importan los módulos necesarios
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
-  title = 'ejercicio7';
+export class AppComponent implements OnInit {
+  posts: any[] = []; // Variable para almacenar los datos
+
+  constructor(private http: HttpClient) {}
+
+  ngOnInit(): void {
+    this.http.get('https://jsonplaceholder.typicode.com/posts').subscribe(
+      (data) => {
+        this.posts = data as any[];
+        console.log('Datos obtenidos:', this.posts); // Depuración
+      },
+      (error) => {
+        console.error('Error al obtener los datos:', error);
+      }
+    );
+  }
 }
